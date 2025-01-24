60d68d1b465d4d8d2aa70ff97aec0842709af0ff
Jbamenorfe <jbamenorfe@gmail.com>
2025-01-24 03:15:26 +0000

    Takes an input from the user and returns its NATO phonetic codes used to facilitate ease of spelling the word.

---
+++ main.py
@@ -0,0 +1,19 @@
+"""This program takes an input from the user and displays its NATO phonetic codes. By JBAmenorfe on 24/01/2025"""
+import pandas
+
+#TODO 1. Create a dictionary in this format {"A": "Alfa", "B": "Bravo"}
+####    (a) Read csv file and assign it to alphabets_df (alphabets dataframe)
+alphabets_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+####    (b) Create the dictionary called alphabets_dict
+alphabets_dict = {row.letter:row.code for (index, row) in alphabets_df.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+####    (a) Receive an input from the user and assign it to a variable called word_to_spell
+word_to_spell = input("Enter a word to spell: ").upper()
+
+####    (b) Create a list of the phonetic code words from the variable word_to_spell
+phonetic_word_list = [alphabets_dict[letter] for letter in word_to_spell]
+print(phonetic_word_list)
+
+
