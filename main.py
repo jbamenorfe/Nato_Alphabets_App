65a3af80972b9747b47b8160d710137b72c4feef
Jbamenorfe <jbamenorfe@gmail.com>
2025-01-31 03:25:53 +0000

    Takes an input from the user and returns its NATO phonetic codes used to facilitate ease of spelling the word. Exceptions have been handled to cater for wrong entries from user

--- main.py
+++ main.py
@@ -10,10 +10,21 @@ alphabets_dict = {row.letter:row.code for (index, row) in alphabets_df.iterrows(
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 ####    (a) Receive an input from the user and assign it to a variable called word_to_spell
-word_to_spell = input("Enter a word to spell: ").upper()
+####    ##### Use exception handling to check the entry made by the user
 
-####    (b) Create a list of the phonetic code words from the variable word_to_spell
-phonetic_word_list = [alphabets_dict[letter] for letter in word_to_spell]
-print(phonetic_word_list)
+def generate_phonetic():
+    word_to_spell = input("Enter a word to spell: ").upper()
+    ####    (b) Create a list of the phonetic code words from the variable word_to_spell
+    try:
+        phonetic_word_list = [alphabets_dict[letter] for letter in word_to_spell]
+    except KeyError:
+        print("Sorry, numbers are not valid entries. Enter only letters!")
+        generate_phonetic()
+    else:
+        print(phonetic_word_list)
+
+generate_phonetic()
+
+
 
 
